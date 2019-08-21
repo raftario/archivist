cdcf5e0707e0dd6777fa9e55136ec8ae6508a849
Raphaël Thériault <raphael_theriault@outlook.com>
2019-08-21 11:48:58 -0400

    Fix build script

--- archivist_compress/src/lib.rs
+++ archivist_compress/src/lib.rs
@@ -1,3 +1,19 @@
+use std::io;
+use std::io::Read;
+
+fn _read_to_bytes<T: Read>(mut source: T) -> Result<Vec<u8>, io::Error> {
+    let mut buffer = Vec::new();
+    source.read_to_end(&mut buffer)?;
+
+    Ok(buffer)
+}
+
+pub mod bz2 {}
+
+pub mod gz {}
+
+pub mod xz {}
+
 #[cfg(test)]
 mod tests {
     #[test]
