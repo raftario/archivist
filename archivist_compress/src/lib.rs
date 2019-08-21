cd87d4291c96573709a1bb9c8cfbdfb366f3afab
Raphaël Thériault <raphael_theriault@outlook.com>
2019-08-21 12:17:20 -0400

    Helper functions

--- archivist_compress/src/lib.rs
+++ archivist_compress/src/lib.rs
@@ -1,11 +1,19 @@
+use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
-fn _read_to_bytes<T: Read>(mut source: T) -> Result<Vec<u8>, io::Error> {
+fn _read_file_to_bytes(path: &str) -> io::Result<Vec<u8>> {
     let mut buffer = Vec::new();
-    source.read_to_end(&mut buffer)?;
+    let mut file = File::open(path)?;
+    file.read_to_end(&mut buffer)?;
 
     Ok(buffer)
+}
+
+fn _write_bytes_to_file(path: &str, buffer: Vec<u8>) -> io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(&buffer)?;
+    file.flush()
 }
 
 pub mod bz2 {}
