76b89c25f96296362b6d31d0d26f61da61c4c449
Raphaël Thériault <raphael_theriault@outlook.com>
2019-08-21 10:43:17 -0400

    CI

---
+++ ci/script.sh
@@ -0,0 +1,24 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --all --target $TARGET
+    cross test --all --target $TARGET --release
+
+    cross clippy --all --target $TARGET -- -D warnings
+
+    cross fmt --all -- --check
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
