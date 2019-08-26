022f8946142cb45ea2c0a060829986cdc6951147
Raphaël Thériault <raphael_theriault@outlook.com>
2019-08-26 08:42:56 -0400

    xz & bz2 (de)compression

--- archivist_compress/src/lib.rs
+++ archivist_compress/src/lib.rs
@@ -1,26 +1,68 @@
-use std::fs::File;
-use std::io;
-use std::io::{Read, Write};
+// TODO: Merge duplicate code
 
-fn _read_file_to_bytes(path: &str) -> io::Result<Vec<u8>> {
-    let mut buffer = Vec::new();
-    let mut file = File::open(path)?;
-    file.read_to_end(&mut buffer)?;
+pub mod bz2 {
+    use bzip2::read::{BzDecoder, BzEncoder};
+    use bzip2::Compression;
+    use std::fs::File;
+    use std::io;
 
-    Ok(buffer)
+    pub fn compress(source: &str, dest: &str, level: u32) -> io::Result<u64> {
+        let level = match level {
+            0 => Compression::Fastest,
+            1 => Compression::Default,
+            2 => Compression::Best,
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "invalid compression level",
+                ));
+            }
+        };
+        let dest_file = File::create(dest)?;
+        let mut dest_compressor = BzEncoder::new(dest_file, level);
+        let mut source_file = File::open(source)?;
+
+        io::copy(&mut source_file, &mut dest_compressor)
+    }
+
+    pub fn decompress(source: &str, dest: &str) -> io::Result<u64> {
+        let source_file = File::open(source)?;
+        let mut source_decompressor = BzDecoder::new(source_file);
+        let mut dest_file = File::create(dest)?;
+
+        io::copy(&mut source_decompressor, &mut dest_file)
+    }
 }
-
-fn _write_bytes_to_file(path: &str, buffer: Vec<u8>) -> io::Result<()> {
-    let mut file = File::create(path)?;
-    file.write_all(&buffer)?;
-    file.flush()
-}
-
-pub mod bz2 {}
 
 pub mod gz {}
 
-pub mod xz {}
+pub mod xz {
+    use std::fs::File;
+    use std::io;
+    use xz2::read::{XzDecoder, XzEncoder};
+
+    pub fn compress(source: &str, dest: &str, level: u32) -> io::Result<u64> {
+        if level > 9 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "invalid compression level",
+            ));
+        }
+        let dest_file = File::create(dest)?;
+        let mut dest_compressor = XzEncoder::new(dest_file, level);
+        let mut source_file = File::open(source)?;
+
+        io::copy(&mut source_file, &mut dest_compressor)
+    }
+
+    pub fn decompress(source: &str, dest: &str) -> io::Result<u64> {
+        let source_file = File::open(source)?;
+        let mut source_decompressor = XzDecoder::new(source_file);
+        let mut dest_file = File::create(dest)?;
+
+        io::copy(&mut source_decompressor, &mut dest_file)
+    }
+}
 
 #[cfg(test)]
 mod tests {
