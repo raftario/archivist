f00bafe594b8b6fcd454354d8d31356dbafe62e2
Raphaël Thériault <raphael_theriault@outlook.com>
2019-09-14 18:22:58 -0400

    Added hash function

--- archivist_hash/src/lib.rs
+++ archivist_hash/src/lib.rs
@@ -1,3 +1,15 @@
+use digest::Digest;
+use std::fs::File;
+use std::io;
+use std::io::Write;
+
+fn _hash_file<D: Digest + Write>(path: &str) -> io::Result<Vec<u8>> {
+    let mut hasher = D::new();
+    let mut file = File::open(path)?;
+    io::copy(&mut file, &mut hasher)?;
+    Ok(hasher.result().to_owned().to_vec())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
